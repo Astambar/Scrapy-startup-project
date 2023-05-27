03b367105bdcc65ee009b0f6296f182fa521b29c
Abdulmajid NASSER <aboud2008@gmail.com>
2023-05-28 00:00:28 +0200

    Template project ready for tuto

---
+++ scrapy_startup_spider/spiders/quotes.py
@@ -0,0 +1,10 @@
+import scrapy
+from scrapy_startup_spider.items import QuoteItem
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        pass
